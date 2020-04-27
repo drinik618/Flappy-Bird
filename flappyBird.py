0309abdd50330dcf7153ceb1f6148b97bbe79a7d
“drinik618” <rinik.dylan18@gmail.com>
2020-04-27 12:07:34 -0400

    Bird is flying

--- flappyBird.py
+++ flappyBird.py
@@ -53,7 +53,7 @@ class Bird:
             if self.tilt > -90:
                 self.tilt -= self.roationVelocity
 
-    def draw(self, win):
+    def draw(self, window):
         self.imageCount += 1
 
         if self.imageCount < self.animationTime:
@@ -74,18 +74,98 @@ class Bird:
 
         rotatedImage = pygame.transform.rotate(self.image, self.tilt)
         newRectangle = rotatedImage.get_rect(center=self.image.get_rect(topleft = (self.x, self.y)).center)
-        win.blit(rotatedImage, newRectangle.topleft)
+        window.blit(rotatedImage, newRectangle.topleft)
 
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
-def draw_window(window, bird):
+
+class Pipe:
+    gap = 200
+    velocity = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.pipeTop = pygame.transform.flip(pipeImage, False, True)
+        self.pipeBottom = pipeImage
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.pipeTop.get_height()
+        self.bottom = self.height + self.gap
+
+    def move(self):
+        self.x -= self.velocity
+
+    def draw(self, window):
+        window.blit(self.pipeTop, (self.x, self.top))
+        window.blit(self.pipeBottom, (self.x, self.bottom))
+
+    def collid(self, bird):
+        birdMask = bird.get_mask()
+        topMask = pygame.mask.from_surface(self.pipeTop)
+        bottomMask = pygame.mask.from_surface(self.pipeBottom)
+
+        topOffset = (self.x - bird.x, self.top - round(bird.y))
+        bottomOffset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        bottomPoint = birdMask.overlap(bottomMask, bottomOffset)
+        topPoint = birdMask.overlap(topMask, topOffset)
+
+        if bottomPoint or bottomPoint:
+            return True
+        return False
+
+
+class Ground:
+    velocity = 5
+    width = groundImage.get_width()
+    image = groundImage
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.width
+
+    def move(self):
+        self.x1 -= self.velocity
+        self.x2 -= self.velocity
+
+        if self.x1 + self.width < 0:
+            self.x1 = self.x2 + self.width
+
+        if self.x2 + self.width < 0:
+            self.x2 = self.x1 + self.width
+
+    def draw(self, window):
+        window.blit(self.image, (self.x1, self.y))
+        window.blit(self.image, (self.x2, self.y))
+
+
+
+def draw_window(window, bird, pipes, ground):
     window.blit(backgroundImage, (0, 0))
+
+    for pipe in pipes:
+        pipe.draw(window)
+
+    ground.draw(window)
+
     bird.draw(window)
     pygame.display.update()
 
+
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    ground = Ground(730)
+    pipes = [Pipe(700)]
     window = pygame.display.set_mode((windowWidth, windowHeight))
     clock = pygame.time.Clock()
 
@@ -96,10 +176,12 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        bird.move()
-        draw_window(window, bird)
+        #bird.move()
+        ground.move()
+        draw_window(window, bird, pipes, ground)
 
     pygame.quit()
     quit()
+
 
 main()
